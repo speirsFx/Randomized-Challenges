b8c2692c9d754120e8e9e087adc97152e5cbbae4
Akash Dixit <31432881+speirsFx@users.noreply.github.com>
2022-09-17 18:56:09 +0530

    Reverse Recursive List

---
+++ Linked List/Problems/recursiveReverse.java
@@ -0,0 +1,18 @@
+class reverseRecursive {
+
+  public static ListNode reverseList(ListNode head){
+
+    if(head == null || head.next == null){
+      return head;
+    }
+
+    ListNode newHead = reverseList(head.next);
+
+    head.next.next = head;
+    head.next = null;
+
+    return newHead;
+
+  }
+
+}
