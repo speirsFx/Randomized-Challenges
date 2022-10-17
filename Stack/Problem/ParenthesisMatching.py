619a1e5e9af4c99aa63806509e09f5013ca153c2
Akash Dixit <31432881+speirsFx@users.noreply.github.com>
2022-10-17 23:42:17 +0530

    Parenthesis Matching Leetcode

--- Stack/Problem/ParenthesisMatching.py
+++ Stack/Problem/ParenthesisMatching.py
@@ -1,3 +1,19 @@
 class ParenthesisMatching(object):
     def isValid(self, s):
-        
+        """
+        :type s: str
+        :rtype: bool
+        """
+        li = []
+        for i in s:
+            if i in ['[', '(', '{']:
+                li.append(i)
+            if i in [']', ')', '}']:
+                if len(li) == 0:
+                    return False
+                if abs(ord(i) - ord(li.pop())) > 2:
+
+                    return False
+        if len(li) > 0:
+            return False
+        return True
