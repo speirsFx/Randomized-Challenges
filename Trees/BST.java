55ab8b6a6dda658bd38806ec54ba07b14ae0e39a
Akash Dixit <31432881+speirsFx@users.noreply.github.com>
2023-01-28 22:41:24 +0530

    BST delete

--- Trees/BST.java
+++ Trees/BST.java
@@ -39,6 +39,72 @@ static Node root;
 
   }
 
+// DELETE THE NODE
+// TO DELETE THE NODE WE NEED INORDER DECESSOR AND INORDER SUCCESSOR
+
+  public static Node InPre(Node p){
+    if( p != null && p.lchild != null )
+          InPre(p.lchild);
+      return p;
+  }
+
+  public static Node InSucc(Node p){
+    if( p != null && p.rchild != null )
+          InSucc(p.rchild);
+      return p;
+  }
+
+
+  public static int height(Node p){
+
+      int x,y;
+
+      if(p == null){
+        return 0;
+      }
+      x = height(p.rchild);
+      y = height(p.lchild);
+
+      if(x > y){
+        return x + 1;
+      }
+      else return y + 1;
+  }
+
+
+  public static Node Delete(Node p, int key){
+
+    Node q;
+      if(p == null){
+        return null;
+      }
+
+
+      if(p.data > key){
+          p.lchild = Delete(p.lchild, key);
+      }
+      else if ( p.data < key){
+        p.rchild = Delete(p.rchild, key);
+      }
+
+      else {
+
+        if(height(p.lchild) > height(p.rchild)){
+          q = InPre(p.lchild);
+          p.data = q.data;
+          p.lchild = Delete(p.lchild, q.data);
+        }
+        else {
+          q = InSucc(p.rchild);
+          p.data = q.data;
+          p.rchild = Delete(p.rchild, q.data);
+        }
+      }
+
+      return p;
+
+  }
+
 
   public static void main(String[] args) {
 
