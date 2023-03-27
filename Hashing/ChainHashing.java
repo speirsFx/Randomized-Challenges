43637d5ca658604b939104f194485443163e6d3a
Akash Dixit <31432881+speirsFx@users.noreply.github.com>
2023-03-27 12:30:17 +0530

    Chain hashing

---
+++ Hashing/ChainHashing.java
@@ -0,0 +1,68 @@
+
+
+class chainHashing {
+
+    static Node arr[] = new Node[10];
+
+
+
+    static void insert(int data){
+
+        int hf = data %10;
+
+        Node newNode = new Node(data);
+
+        if(arr[hf] == null){
+            arr[hf] = newNode;
+        }
+        else {
+            if(arr[hf].data >= data){
+                Node node = arr[hf];
+                arr[hf] = newNode;
+                newNode.next = node;
+                return;
+            }
+            else {
+                if(arr[hf].next == null){
+                    arr[hf].next = newNode;
+                    return;
+                }
+                Node node = arr[hf].next;
+                Node n = node;
+                while(node != null && node.data <= data){
+                    n = node;
+                    node = node.next;
+                }
+                newNode.next = n.next;
+                n.next = newNode;
+                return;
+            }
+
+            
+        }
+        
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = {1,2,2,3,4,5,3,5,55,10,12,555,75,87,85,105,985};
+
+        for(int i: a){
+            insert(i);
+        }
+
+      Node node = arr[5];
+      while(node != null){
+        System.out.println(node.data);
+        node = node.next;
+      }  
+      
+
+
+    }
+
+
+
+
+}
+
---
+++ Hashing/Node.java
@@ -0,0 +1,16 @@
+public class Node {
+
+    Node next;
+    int data;
+
+    Node(){
+        next = null;
+    }
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+    
+}
